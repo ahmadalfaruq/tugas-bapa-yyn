e7ef2cc9b4378c6e88a26c07f5f8ce5decb8d241
ahmadalfaruq <ahmadalfaruq33@gmail.com>
2019-11-23 03:54:07 +0700

    upload

---
+++ Latihan1 menampilkan/src/menampilkan/Menampilkan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menampilkan;
+
+/**
+ Nama : ahmad al faruq 
+ KELAS : TI-1C
+ NIM : A2.1900010
+ */
+public class Menampilkan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata Diri Sendiri" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: ahmad al faruq");
+        System.out.println("ALAMAT\t\t: desa cikoneng RT 02 RW 04");
+        System.out.println("TEMPAT LAHIR\t: bandung ");
+        System.out.println("TANGGAL LAHIR\t: 02 oktober 2000");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: main game");
+        System.out.println("MAKES\t\t: nasi");
+        System.out.println("MIKES\t\t: susu murni");
+    }
+}
